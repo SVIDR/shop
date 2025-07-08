e13299a5d48a3269f99577eb790043cabfd7d08b
SVIDR <rsvidovits@mail.ru>
2025-07-08 20:47:14 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ProductPage from "./assets/ProductPage"
+import NavigationBar from "./NavigationBar"
+import Products from "./Products"
+
+function App() {
+
+  return (
+    <>
+      <NavigationBar />
+      {/* <Products /> */}
+      <ProductPage />
+    </>
+  )
+}
+
+export default App
